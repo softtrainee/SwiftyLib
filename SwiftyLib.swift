a616df47f142f60625e0c7b322d533d27accb7ec
Mohit <mohit_4545@rediffmail.com>
2019-03-04 18:35:20 +0630

    Added test cases

---
+++ SwiftyLib.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Mohit on 3/4/19.
+//  Copyright © 2019 Mohit. All rights reserved.
+//
+
+public final class SwiftyLib {
+    
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- SwiftyLibTests/SwiftyLibTests.swift
+++ SwiftyLibTests/SwiftyLibTests.swift
@@ -10,11 +10,22 @@ import XCTest
 @testable import SwiftyLib
 
 class SwiftyLibTests: XCTestCase {
+    
+    var swiftyLib: SwiftyLib!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        swiftyLib = SwiftyLib()
     }
 
+    func testAdd() {
+        XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
+    }
+    
+    func testSub() {
+        XCTAssertEqual(swiftyLib.sub(a: 2, b: 1), 1)
+    }
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
