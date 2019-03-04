2046b23dad54394b4b8281dfc0a97cf459749d41
Mohit <mohit_4545@rediffmail.com>
2019-03-04 17:43:57 +0630

    Intital project setup

---
+++ SwiftyLibExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyLibExamples
+//
+//  Created by Mohit on 3/4/19.
+//  Copyright © 2019 Mohit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
